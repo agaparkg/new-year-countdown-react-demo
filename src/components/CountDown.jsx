916e1d7160c431bf412a5f9e4ff17e07dc91844d
Azamat Gaparov <agaparovkg@gmail.com>
2024-05-08 17:29:46 -0700

    new changes

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -1,16 +1,35 @@
+import { useRef, useState } from "react";
 import Days from "./Days";
 import Hours from "./Hours";
 import Minutes from "./Minutes";
 import Seconds from "./Seconds";
 
 const CountDown = ({ currYear }) => {
+  const intervalRef = useRef(null);
+
+  const [timeObj, setTimeObj] = useState({
+    d: 0,
+    h: 0,
+    m: 0,
+    s: 0,
+  });
+
+  //   d, h, m, s
+
+  const handleStart = () => {};
+
   return (
-    <div className="countdown">
-      <Days />
-      <Hours />
-      <Minutes />
-      <Seconds />
-    </div>
+    <>
+      <div className="countdown">
+        <Days />
+        <Hours />
+        <Minutes />
+        <Seconds />
+      </div>
+      <button onClick={handleStart} className="start-btn">
+        Start
+      </button>
+    </>
   );
 };
 
